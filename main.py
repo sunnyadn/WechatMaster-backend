001d4ef0fd074b567bb10950e24a13dcbc7059f5
Sunny Young <sunny@mogara.org>
2016-04-22 00:42:04 +0800

    init

---
+++ main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+from hashlib import sha1
+
+import weclient
+
+client = weclient.Client()
+
+class LoginHandler(tornado.web.RequestHandler):
+    # def get(self):
+    #     self.write("Hello, world")
+    #     client.sendMsg("ogCtWv9jhHIhvgF27NVIxSjgqjn4", "LoggedIn")
+
+    def post(self):
+        name = self.get_argument("name")
+        pwd = self.get_argument("pwd")
+        imgcode = self.get_argument("imgcode")
+
+        client.login(name, pwd, imgcode)
+
+        app_id = self.get_argument("app_id")
+        app_secret = self.get_argument("secret")
+
+        wechat.setAppInfo(app_id, app_secret)
+
+class WeChatHandler(tornado.web.RequestHandler):
+    def get(self):
+        signature = self.get_argument("signature")
+        timestamp = self.get_argument("timestamp")
+        nonce = self.get_argument("nonce")
+        echostr = self.get_argument("echostr")
+
+        if wc.checkSignature(signature, timestamp, nonce, echostr):
+            self.write(echostr)
+
+    def post(self):
+        msg_type = self.get_argument("MsgType")
+        source = self.get_argument("FromUserName")
+        if msg_type == "text":
+            content = self.get_argument("Content")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/login", LoginHandler),
+        (r"/wechat", WeChatHandler),
+    ])
+
+if __name__ == "__main__":
+    # app = make_app()
+    # app.listen(8888)
+    # tornado.ioloop.IOLoop.current().start()
+    client.login("nuistsunny", "SunnyYoung1130")
+    client.sendMsg("ogCtWv9jhHIhvgF27NVIxSjgqjn4", "LoggedIn")
---
+++ weclient.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# import tornado.httpclient
+import requests
+
+from hashlib import md5
+from urllib import urlencode
+import re
+import random
+
+class Client(object):
+    def __init__(self):
+        self.httpclient = requests.session()
+
+    def login(self, name, pwd, imgcode = ""):
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.8",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer": "https://mp.weixin.qq.com/",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+        pwd = md5(pwd[0:16]).hexdigest()
+        print pwd
+        body = {
+            "username": name,
+            "pwd": pwd,
+            "imgcode": imgcode,
+            "f": "json"
+        }
+        # req = tornado.httpclient.HTTPRequest("https://mp.weixin.qq.com/cgi-bin/login", "POST", headers, body)
+
+        # self.httpclient.fetch(req, self.saveToken)
+        res = self.httpclient.post("https://mp.weixin.qq.com/cgi-bin/login", headers = headers, data = body)
+        body = eval(res.text)
+        print body
+        url = body["redirect_url"]
+        self.token = re.findall(r".+?=(\d+)$", url)[0]
+        print self.token
+
+    def saveToken(self, res):
+        body = eval(res.body)
+        print body
+        url = body["redirect_url"]
+        self.token = re.findall(r".+?=(\d+)$", url)[0]
+        print self.token
+
+    def sendMsg(self, target, msg, imgcode = ""):
+        query = {
+            "t": "ajax-response",
+            "f": "json",
+            "token": self.token,
+            "lang": "zh_CN"
+        }
+
+        url = "https://mp.weixin.qq.com/cgi-bin/singlesend?" + urlencode(query)
+        print url
+
+        headers = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.8",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer": "https://mp.weixin.qq.com/cgi-bin/message?t=message/list&count=20&day=7&token=" + self.token + "&lang=zh_CN",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+
+        body = {
+            "token": self.token,
+            "lang": "zh_CN",
+            "f": "json",
+            "ajax": "1",
+            "random": "0.4150608658290731",
+            "mask": "false",
+            "tofakeid": target,
+            "imgcode": "",
+            "type": "1",
+            "content": msg,
+            "appmsg": "",
+        }
+
+        print body
+
+        res=self.httpclient.post(url,data=body,headers=headers)
+        print res.text
+
+    def onMessageSent(self, res):
+        print res.body
